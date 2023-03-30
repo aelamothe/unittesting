5f86ee64c2c10726676506babf30bf9909183cae
anasvassia <alamothe@college.harvard.edu>
2023-03-29 23:42:59 -0400

    unit tests added

--- greet.js
+++ greet.js
@@ -1,4 +1,19 @@
 
 function greet(name) {
-    return "Hello " + name;
+    let statement = "";
+    if (typeof(name) == 'string') {
+        if (name == name.toUpperCase()) {
+            statement = "HELLO " + name;
+        }
+        else {
+            statement = "Hello " + name;
+        }
+    }
+    else if (Array.isArray(name)) {
+        statement = "Hello " + name.join(', ');
+    }
+    else {
+        statement = "Hello there";
+    }
+    return statement;
 }
--- greet.test.js
+++ greet.test.js
@@ -3,7 +3,19 @@ var greet = utils.load('./greet.js', 'greet');
 
 describe('Greet', () => {
 
+    test('Null value', () => {
+        expect(greet(null)).toBe('Hello there');
+    });
     test('Single name', () => {
         expect(greet("Elizabeth")).toBe('Hello Elizabeth');
+    });
+    test('Shouting name', () => {
+        expect(greet('JOSE')).toBe('HELLO JOSE');
+    });
+    test('Two names', () => {
+        expect(greet(['Jose','Pep'])).toBe('Hello Jose, Pep');
+    });
+    test('Many names', () => {
+        expect(greet(['Alex', 'Arsene', 'Jose', 'Zidane'])).toBe('Hello Alex, Arsene, Jose, Zidane');
     });
 });
